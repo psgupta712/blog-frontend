c4b8af1627f3f5c61210b7fa6896e75e299a241d
Priyanshu Gupta <118977399+psgupta712@users.noreply.github.com>
2024-10-05 18:44:57 +0530

    Update Auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,7 +16,7 @@ function Auth() {
       // api request to login or register
 
       const { data } = await axios.post(
-        `https://blogging-backend-oh2u.onrender.com/api/users${isRegister ? "" : "/login"}`,
+        `https://blog-backend-r2qa.onrender.com/api/users${isRegister ? "" : "/login"}`,
         { user: values }
       );
 
