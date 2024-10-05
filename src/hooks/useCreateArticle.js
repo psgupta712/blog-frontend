3b969290a272b5505ab2fd3ead315603c4aa6f07
Priyanshu Gupta <118977399+psgupta712@users.noreply.github.com>
2024-10-05 18:42:30 +0530

    Update useCreateArticle.js

--- src/hooks/useCreateArticle.js
+++ src/hooks/useCreateArticle.js
@@ -7,7 +7,7 @@ const creteArticle = async (values) => {
     console.log("creteArticle", { values });
 
     const { data } = await axios.post(
-        `https://blogging-backend-oh2u.onrender.com/api/articles`,
+        `https://blog-backend-r2qa.onrender.com/api/articles`,
         { article: {...values.values} }
       );
 
